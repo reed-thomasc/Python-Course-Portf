6a5daceb1deb7888b38947b619ef9a3697482454
reed-thomasc <156951224+reed-thomasc@users.noreply.github.com>
2024-03-13 10:42:22 -0400

    Add files via upload

---
+++ 100Days_Day_1_Project.py
@@ -0,0 +1,10 @@
+#greeting statement
+print("WELCOME TO THE BAND NAME GENERATOR!")
+#city input statement
+city = input("What city did you grow up in?\n")
+#pet name input statement
+pet_name = input("What is the name of a pet?\n")
+#print statement
+print("Your band name could be " + city + " " + pet_name)
+
+
